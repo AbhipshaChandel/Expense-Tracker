e9f9cefb58dc271c94b0936f7bc92ee9217ebd0c
AbhipshaChandel <chandelabhipsha0007@gmail.com>
2026-09-10 20:18:37 +0530

    Login and Register for User Authetication added on server side

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,10 @@ dns.setServers(["8.8.8.8"]);
 const express = require("express");
 const mongoose = require("mongoose");
 const cors=require("cors")
+const bcrypt=require("bcryptjs")
+const jwt=require("jsonwebtoken")
+const User=require("./models/user")
+
 require("dotenv").config();
 const Transaction=require("./models/transaction")
 
@@ -26,6 +30,87 @@ mongoose
   .catch((err) => {
     console.error("MongoDB connection failed:", err.message);
   });
+
+//--------------------//USER REGISTER//---------------------------//
+
+  app.post("/api/auth/register",async(req,res)=>{
+    try{
+      const {name,email,password}=req.body
+      if(!name || !email || !password){
+       return res.status(404).json({message:"Provide all information"})
+      }
+
+      const UserExist=await User.findOne({email})    
+      if(UserExist){
+       return res.status(400).json({message:"User already exist"})
+      }
+
+      const hashedPassword=await bcrypt.hash(password,10)
+
+      const user=await User.create({
+        name,
+        email,
+        password:hashedPassword
+      })
+
+      res.status(201).json({
+        messaage:"User registered successfully",
+        user:{
+          id:user._id,
+          name:user.name,
+          email:user.email
+        }
+      })
+    }catch(err){
+      res.status(404).json({message:"Registration failed"})
+    }
+
+  })
+
+
+  //--------------------//USER LOGIN//---------------------------//
+
+  app.post("/api/auth/login",async(req,res)=>{
+    try{
+      const {password,email}=req.body
+      const user=await User.findOne({email})
+
+      if(!user){
+        return res.status(401).json({message:"Invalid email or password"})
+      }
+
+      const checkpassword=await bcrypt.compare(
+        password,user.password
+      )
+
+      if(!checkpassword){
+        return res.status(401).json({message:"Invalid email or password"})
+
+      }
+
+      const Token=jwt.sign(
+        {
+          userID:user._id
+        },
+        process.env.JWT_SECRET,
+        {
+         ExpiresIn:"1D"
+        }
+      )
+
+      res.status(400).json({
+        message:"Login Successfully",token,
+        user:{
+          id:user._id,
+          name:user.name,
+          email:user.email
+        }
+      })
+
+    }catch(err){
+      res.status(500).json({message:"Login Failed"})
+    }
+  })
 
 
 app.get("/api/transaction",async(req,res)=>{
